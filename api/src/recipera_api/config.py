046a92f14464e8e1c09c02b16de5456ec25c9799
Filip Kafka <filip.kafka@gmail.com>
2026-07-12 13:29:26 +0200

    add api pydantic-settings config

---
+++ api/src/recipera_api/config.py
@@ -0,0 +1,31 @@
+from functools import lru_cache
+
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import URL
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+    postgres_user: str
+    postgres_password: SecretStr
+    postgres_db: str
+    postgres_host: str = "localhost"
+    postgres_port: int = 5432
+
+    @property
+    def database_url(self) -> URL:
+        # to get a string representation use: .render_as_string(hide_password=False)
+        return URL.create(
+            drivername="postgresql+psycopg",
+            username=self.postgres_user,
+            password=self.postgres_password.get_secret_value(),
+            host=self.postgres_host,
+            port=self.postgres_port,
+            database=self.postgres_db,
+        )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings() # type: ignore[reportCallIssue]
